a350d6eea1e28cc985e386f0118c0eb202f47267
Vlasis Tassios <49943456+TassiosVlasis@users.noreply.github.com>
2020-09-24 15:00:45 +0300

    Update EditEmployee.js

--- Crud_app/client/components/EditEmployee.js
+++ Crud_app/client/components/EditEmployee.js
@@ -1,73 +1,46 @@
-import React, { Fragment, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const EditEmployee = ({ employee }) => {
-  const [empl, setEmpl] = useState(employee.empl);
+const EditEmployee = (props) => {
+  const [user, setUser] = useState(props.currentUser);
 
-  //update employee
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
 
-  const updateEmployee = async (e) => {
-    e.preventDefault();
-    try {
-      const body = { empl };
-      const response = await fetch(
-        `http://localhost:5000/employee/${employee.emp_id}`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-        }
-      );
-      window.location = "/";
-    } catch (err) {
-      console.error(err.message);
-    }
+    setUser({ ...user, [name]: value });
   };
-
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
   return (
-    <Fragment>
-      <button
-        type="button"
-        class="btn btn-warning"
-        data-toggle="modal"
-        data-target={`#emp_id${employee.emp_id}`}
-      >
-        Edit
-      </button>
-      <div class="modal" emp_id={`emp_id${employee.emp_id}`}>
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Edit Employee</h4>
-              <button type="button" class="close" data-dismiss="modal">
-                &times;
-              </button>
-            </div>
-            <div class="modal-body">
-              <input
-                type="text"
-                className="form-control"
-                value={empl}
-                onChange={(e) => setEmpl(e.target.value)}
-              />
-            </div>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
 
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-warning"
-                data-dismiss="modal"
-                onClick={(e) => updateEmployee(e)}
-              >
-                Edit
-              </button>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Fragment>
+        props.updateUser(user.id, user);
+      }}
+    >
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        value={user.name}
+        onChange={handleInputChange}
+      />
+      <label>Username</label>
+      <input
+        type="text"
+        name="username"
+        value={user.username}
+        onChange={handleInputChange}
+      />
+      <button>Update user</button>
+      <button
+        onClick={() => props.setEditing(false)}
+        className="button muted-button"
+      >
+        Cancel
+      </button>
+    </form>
   );
 };
 
